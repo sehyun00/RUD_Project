aae3532fbb00072de1cb547a488d3f17ce9a7834
JungHyun <145425504+kastor0323@users.noreply.github.com>
2024-10-14 15:37:28 +0900

    부트로 수정

---
+++ rud_boot/src/main/java/com/rud/rud/RudApplication.java
@@ -0,0 +1,13 @@
+package com.rud.rud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RudApplication.class, args);
+	}
+
+}
