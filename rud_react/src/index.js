a43175998f196fbb01a83bbad75a0baabe535c97
김세현 <sh000917@gmail.com>
2024-10-16 14:53:47 +0900

    fix)주소 변경

--- rud_react/src/index.js
+++ rud_react/src/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './assets/scss/style.scss';
 import ReactDOM from 'react-dom/client';
-import { createBrowserHistory } from 'history';
-import { Route, Routes, HashRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Components from './views/components/components.jsx';
 import Login from './views/components/login.jsx';
-import CustomComponents from './views/custom-components/custom-components.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-var hist = createBrowserHistory();
 root.render(
-    <HashRouter history={hist}>
+    <Router>
         <Routes>
             <Route path="/" element={<Login />} /> {/* 기본 페이지를 로그인 페이지로 변경 */}
             <Route path="/home" element={<Components />} />
         </Routes>
-    </HashRouter>
+    </Router>
 );
