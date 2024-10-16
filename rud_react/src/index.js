084a99e65d9fdca9e01ef783e90931e7be271278
Jong6e <aqwer04560@gmail.com>
2024-10-16 17:56:40 +0900

    fix) 버그시발

--- rud_react/src/index.js
+++ rud_react/src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import './assets/scss/style.scss';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { Route, Routes, HashRouter } from 'react-router-dom';
 import Components from './views/components/components.jsx';
 import Login from './views/components/login.jsx';
+import CustomComponents from './views/custom-components/custom-components.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+var hist = createBrowserHistory();
 root.render(
-    <Router>
+    <HashRouter history={hist}>
         <Routes>
-            <Route path="/" element={<Login />} /> {/* 기본 페이지를 로그인 페이지로 변경 */}
-            <Route path="/home" element={<Components />} />
+            {/* <Route path="/custom-components" element={<CustomComponents />} /> */}
+            <Route path="/" element={<Components />} />
+            <Route path="/login" element={<Login />} />
         </Routes>
-    </Router>
+    </HashRouter>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
