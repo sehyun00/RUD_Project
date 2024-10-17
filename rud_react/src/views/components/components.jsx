cf71086df0fd88893b4d74644a1cf97920066891
김세현 <sh000917@gmail.com>
2024-10-17 09:39:40 +0900

    fix) 배너, 헤더 경로 수정

--- rud_react/src/views/components/components.jsx
+++ rud_react/src/views/components/components.jsx
@@ -2,36 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // core components
-import Header from '../../components/header/header.jsx';
-import HeaderBanner from '../../components/banner/banner.jsx';
-import Footer from '../../components/footer/footer.jsx';
+import Header from '../item_components/header.jsx';
 
 // sections for this page
-import Buttons from './sections/buttons.jsx';
-import Labels from './sections/labels.jsx';
-import PagePagination from './sections/pagination.jsx';
-import Images from './sections/images.jsx';
-import Breadcrumbs from './sections/breadcrumbs.jsx';
-import Cards from './sections/cards.jsx';
-import Dropdowns from './sections/dropdowns.jsx';
-import PageForm from './sections/form.jsx';
 import PageTable from './sections/table.jsx';
-import Notification from './sections/notification.jsx';
-import TooltipPopover from './sections/tooltip-popover.jsx';
-import Typography from './sections/typography.jsx';
-import JsComponents from './sections/js-components.jsx';
-import CallToAction from '../../components/call-to-action/CallToAction';
-
-import HeaderBanner2 from '../../components/banner2/banner2.jsx';
 
 // sections for this page
 
-import BannerComponent from '../custom-components/sections/bannercomponent.jsx';
-
-import FeatureComponent from '../custom-components/sections/featurecomponent.jsx';
-import PortfolioComponent from '../custom-components/sections/portfoliocomponent.jsx';
-
-import TeamComponent from '../custom-components/sections/teamcomponent.jsx';
 
 const Components = () => {
     return (
