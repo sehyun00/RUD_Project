556631eea33257155fed000d8e26d8c1c331087c
김세현 <sh000917@gmail.com>
2024-10-18 10:08:29 +0900

    add) 이미지 업로드 페이지

---
+++ rud_react/src/views/components/imageUpload.jsx
@@ -0,0 +1,20 @@
+// feature
+import React from "react";
+import PropType from 'prop-types';
+
+// css
+import '../../assets/css/components/imageUpload.scss'
+
+const ImageUpload =()=>{
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+ImageUpload.PropType ={
+    class:PropType.object
+};
+
+export default ImageUpload;
