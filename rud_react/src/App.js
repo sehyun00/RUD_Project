4c8808d14f095670d13ba089886a417ce226f963
김세현 <sh000917@gmail.com>
2024-10-21 11:20:11 +0900

    fix) 화면 전체 검댕이로 바꿈

--- rud_react/src/App.js
+++ rud_react/src/App.js
@@ -14,13 +14,11 @@ import SignUp from "./views/signup";
 import Header from "./views/componentItems/header";
 
 function App() {
-    const location = useLocation();
-    const isLoginPage = location.pathname === '/login';
     
     // view
     return (
-        <div className={isLoginPage ? "full-screen" : ""}>
-            {!isLoginPage && <Header />} {/* 로그인 페이지가 아닐 때만 Header 표시 */}
+        <div>
+            <Header />
             <Routes>
                 <Route path='/home' element={<Home />} />
                 <Route path='/login' element={<Login />} />
--- rud_react/src/views/componentItems/header.jsx
+++ rud_react/src/views/componentItems/header.jsx
@@ -1,21 +1,15 @@
-// feature
 import React from "react";
 import { Container } from 'reactstrap';
-import { useLocation } from 'react-router-dom'; // useLocation 훅 추가
-
-// scss
+import { useLocation } from 'react-router-dom';
 import '../../assets/css/componentItems/header.scss';
+import happyCatVideo from '../../assets/images/happycat.mp4';
 
-// image
-import happyCatVideo from '../../assets/images/happycat.mp4'; // 비디오 파일 경로
-
-// component
 const Header = () => {
     const location = useLocation();
     const isLoginPage = location.pathname === '/login'; // 로그인 페이지 여부 확인
 
     return (
-        <div className={`header ${isLoginPage ? 'inverted' : ''}`}> {/* 로그인 페이지일 때 클래스 추가 */}
+        <div className="header">
             <Container className="header-container">
                 <div className="left">
                     <video autoPlay loop muted className="logo-video">
@@ -24,15 +18,13 @@ const Header = () => {
                     </video>
                     <h3>Superant</h3>
                 </div>
-                {
-                    !isLoginPage && ( // 로그인 페이지가 아닐 때만 메뉴 표시
-                        <div className="right">
-                            <h5>메뉴1</h5>
-                            <h5>메뉴2</h5>
-                            <h5>메뉴3</h5>
-                        </div>
-                    )
-                }
+                {!isLoginPage && ( // 로그인 페이지가 아닐 때만 메뉴 표시
+                    <div className="right">
+                        <h5>메뉴1</h5>
+                        <h5>메뉴2</h5>
+                        <h5>메뉴3</h5>
+                    </div>
+                )}
             </Container>
         </div>
     );
