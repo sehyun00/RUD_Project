c690b10ddeb20f3de2d54cbee9e5384aee3deecf
qTeTp <68058317+qTeTp@users.noreply.github.com>
2024-11-01 18:04:11 +0900

    fix)종목 월봉 조회 추가

--- rud_stockapi/mojito_server.py
+++ rud_stockapi/mojito_server.py
@@ -41,7 +41,38 @@ def get_daily_kospi():
     )
     resp = broker.fetch_ohlcv(
         symbol=request.args.get('name'),
+        # D = 일봉, W = 주봉, M = 월봉
         timeframe='D',
+        adj_price=True
+    )
+
+    data = resp['output2']
+    result = {}
+
+    for entry in data:
+        date = entry['stck_bsop_date']  # 날짜
+        result[date] = {
+            'open': entry['stck_oprc'],  # 시가
+            'high': entry['stck_hgpr'],   # 고가
+            'low': entry['stck_lwpr'],    # 저가
+            'close': entry['stck_clpr']    # 종가
+        }
+
+    return jsonify(result), 200
+
+
+# 국장 월봉 구하는 메소드 응애 종목들은 조회 안됨
+@app.route('/getmonthlykos', methods=['GET'])
+def get_monthly_kospi():
+    broker = mojito.KoreaInvestment(
+        api_key=key,
+        api_secret=secret,
+        acc_no=acc_no
+    )
+    resp = broker.fetch_ohlcv(
+        symbol=request.args.get('name'),
+        # D = 일봉, W = 주봉, M = 월봉
+        timeframe='M',
         adj_price=True
     )
 
@@ -73,7 +104,6 @@ def get_nasdaq():
             exchange=exchange
         )
         resp = broker.fetch_price(request.args.get('name'))
-        print(resp)
 
         if resp and resp.get('rt_cd') == "0":
             if 'output' in resp and 'last' in resp['output']:
@@ -106,7 +136,54 @@ def get_daily_nasdaq():
             timeframe='D',
             adj_price=True
         )
-        print(resp)
+
+        if resp and resp.get('rt_cd') == "0":
+            data = resp.get('output2', [])
+            if data:  # output2가 비어있지 않은 경우
+                for entry in data:
+                    date = entry['xymd']  # 날짜
+                    result[date] = {
+                        'open': entry['open'],  # 시작가
+                        'high': entry['high'],   # 고가
+                        'low': entry['low'],    # 저가
+                        'close': entry['clos']    # 종가
+                    }
+                break  # 데이터를 찾으면 루프 종료
+            else:
+                print(
+                    f"No data found for {exchange} on {request.args.get('name')}")
+        else:
+            print(f"Error in response for {exchange}: {resp.get('msg1')}")
+        # 호출을 다라락 하면 한투 서버가 막음 많이 터지면 시간 늘리기
+        time.sleep(0.5)
+
+    else:
+        print(f"Error in response for {exchange}: {resp.get('msg1')}")
+
+    if not result:
+        return jsonify({"error": "가격 정보를 찾을 수 없습니다."}), 404
+
+    return jsonify(result), 200
+
+
+# 나스닥 월봉 구하는 메소드
+@app.route('/getmonthlynas', methods=['GET'])
+def get_monthly_nasdaq():
+    exchanges = ['나스닥', '아멕스', '뉴욕']
+    result = {}
+
+    for exchange in exchanges:
+        broker = mojito.KoreaInvestment(
+            api_key=key,
+            api_secret=secret,
+            acc_no=acc_no,
+            exchange=exchange
+        )
+        resp = broker.fetch_ohlcv(
+            symbol=request.args.get('name'),
+            timeframe='M',
+            adj_price=True
+        )
 
         if resp and resp.get('rt_cd') == "0":
             data = resp.get('output2', [])
