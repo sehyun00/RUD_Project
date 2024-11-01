e23edc96c1b024cdd195b887409a10da7cc1d008
sehyun <sh000917@gmail.com>
2024-11-01 17:46:44 +0900

    add) 로그 css

--- rud_react/src/views/components/log.jsx
+++ rud_react/src/views/components/log.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 // scss
-// import '../../assets/css/home.scss';
+import "../../assets/css/components/log.scss"
 
 // page
 const Log = () => {
 
 
     return (
-        <div>
+        <div className="converter-container"> {/* 스타일 적용된 컨테이너 */}
+            <div className="files-container"> {/* 파일 컨테이너 */}
             <h1>응애</h1>
             <h1>응애</h1>
             <h1>응애</h1>
             <h1>응애</h1>
+
+            </div>
         </div>
     );
 };
