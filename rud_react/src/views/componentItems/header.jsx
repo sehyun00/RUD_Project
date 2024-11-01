3f6e1060c18065467fb34794bc19d453c7bcd08c
sehyun <sh000917@gmail.com>
2024-11-01 18:22:02 +0900

    no message

--- rud_react/src/views/componentItems/header.jsx
+++ rud_react/src/views/componentItems/header.jsx
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
 import React, {useState} from "react";
 import {Container} from 'reactstrap';
 import {useLocation, Link} from 'react-router-dom';
-=======
-import React from "react";
-import { Container } from 'reactstrap';
-<<<<<<< HEAD
-import { useLocation } from 'react-router-dom';
-=======
-import { useLocation, Link } from 'react-router-dom';
->>>>>>> 560fc468d239ac9da74978501b964146485ae17c
->>>>>>> origin/back)Member_DB
 import '../../assets/css/componentItems/header.scss';
 import happyCatVideo from '../../assets/images/happycat.mp4';
 
@@ -26,13 +16,9 @@ const Header = () => {
                         <source src={happyCatVideo} type="video/mp4"/>
                         Your browser does not support the video tag.
                     </video>
-<<<<<<< HEAD
-                    <h3>Superant</h3>
-=======
                     <Link to="/home">
                         <h3>Superant</h3>
                     </Link>
->>>>>>> 560fc468d239ac9da74978501b964146485ae17c
                 </div>
                 {
                     !isLoginPage && (
