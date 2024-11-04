cbb490153d03e526c8ebebe6a2b5ad91962c9b51
JungHyun <145425504+kastor0323@users.noreply.github.com>
2024-11-04 18:08:50 +0900

    rud service작성

--- rud_boot/src/main/java/com/rud/rud/rud/Rud.java
+++ rud_boot/src/main/java/com/rud/rud/rud/Rud.java
@@ -6,19 +6,32 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
 @Getter
-@Service
+@Setter
 @Entity
 public class Rud {
     @Id
-    private Date rebalancingdate;
+    private Date rebalancingDate;
 
     @OneToOne
     @JoinColumn(name = "USER_ID")
     private Member userId;
+
+    private String won;
+
+    private String dollar;
+
+    private String stockName;
+
+    private double nos;
+
+    private double marketOrder;
+
+    private double expertPer;
 
 }
--- rud_boot/src/main/java/com/rud/rud/rud/RudController.java
+++ rud_boot/src/main/java/com/rud/rud/rud/RudController.java
@@ -1,9 +1,12 @@
 package com.rud.rud.rud;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/superant")
 public class RudController {
     
     //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡOCR 배열 받아서 표로 보여주기ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
@@ -11,7 +14,9 @@ public class RudController {
 
 
     //ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ로그 db에 저장ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-    @PostMapping("/rud")
+    @GetMapping("/rud")
+
+    @PostMapping("/rud/{reblancingDate}")
     public String saveRud() {
         return "rud";
     }
