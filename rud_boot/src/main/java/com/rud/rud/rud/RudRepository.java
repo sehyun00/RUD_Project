fe46dbaee2ed212640f4a3609ddf5109f0b3bd9e
JungHyun <145425504+kastor0323@users.noreply.github.com>
2024-11-05 15:36:34 +0900

    no message

--- rud_boot/src/main/java/com/rud/rud/rud/RudRepository.java
+++ rud_boot/src/main/java/com/rud/rud/rud/RudRepository.java
@@ -1,6 +1,8 @@
 package com.rud.rud.rud;
 
+import com.rud.rud.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RudRepository extends JpaRepository {
+public interface RudRepository extends JpaRepository<Rud, String> {
 }
+
