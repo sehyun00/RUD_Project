59f16116e25885ea8afcddc49d852025870fc42d
JungHyun <145425504+kastor0323@users.noreply.github.com>
2024-11-06 18:06:43 +0900

    test1

--- rud_boot/src/main/java/com/rud/rud/rud/Rud.java
+++ rud_boot/src/main/java/com/rud/rud/rud/Rud.java
@@ -1,24 +1,34 @@
 package com.rud.rud.rud;
 
 import com.rud.rud.member.Member;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.Getter;
-import org.springframework.stereotype.Service;
+import lombok.Setter;
 
-import java.util.Date;
+import java.sql.Date;
+import java.time.LocalDateTime;
 
 @Getter
-@Service
+@Setter
 @Entity
 public class Rud {
     @Id
-    private Date rebalancingdate;
+    private LocalDateTime rebalancingDate; //rud일자
 
-    @OneToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "USER_ID")
-    private Member userId;
+    private Member userId; //유저 아이디
+
+    private String won; //원화
+
+    private String dollar; //달러
+
+    private String stockName; //주식명
+
+    private double nos; //수량
+
+    private double marketOrder; //주식가격
+
+    private double expertPer; //희망비중
 
 }
--- rud_boot/src/main/java/com/rud/rud/rud/RudRepository.java
+++ rud_boot/src/main/java/com/rud/rud/rud/RudRepository.java
@@ -2,5 +2,5 @@ package com.rud.rud.rud;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RudRepository extends JpaRepository {
+public interface RudRepository extends JpaRepository<Rud, String> {
 }
